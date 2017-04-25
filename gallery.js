507b9870fcdb303cd367263d463115a4fd314504
mistakuaku <dragonz139@hotmail.com>
2017-04-25 13:34:40 -0700

    Lab 24 finished and working properly although the code is pretty ugly.

--- gallery.js
+++ gallery.js
@@ -1,6 +1,23 @@
-
+var chico = ["Selvester's Cafe", "The Rec", "O'Connel", "Meriam Library", "Glenn Hall"];
+var current;
 
 function shuffleGallery()
 {
+
+  var len = chico.length;
+  var randInt = Math.floor(Math.random() * len);
+  //console.log(randInt);
+  //console.log(current);
+  if (randInt == current)
+  {
+    while(randInt == current)
+    {
+      randInt = Math.floor(Math.random() * len);
+    }
+  }
+
+  current = randInt;
+  console.log(chico[randInt]);
+
 
 }
