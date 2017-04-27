674bcf57e60b2ff1c7098bbebe933add9d741d7b
mistakuaku <dragonz139@hotmail.com>
2017-04-27 14:07:04 -0700

    Meant to update this when you said last time but anyways here is the final product that works.

--- gallery.js
+++ gallery.js
@@ -1,5 +1,28 @@
-var chico = ["Selvester's Cafe", "The Rec", "O'Connel", "Meriam Library", "Glenn Hall"];
+var chico = [
+   {description: "Selvester's Cafe",
+    picture: "https://www.csuchico.edu/maps/images/buildings-photos/buildings-selvestors.jpg",
+    from: "csuchico.edu"
+   },
+   {description: "The Rec",
+   picture: "https://www.csuchico.edu/pdc/images/CP-2009-WREC/WREC-at-night.jpg",
+   from: "csuchico.edu"
+   },
+   {description: "O'Connel",
+    picture: "http://www.csuchico.edu/maps/images/buildings-photos/buildings-oconnell.jpg",
+    from: "csuchico.edu"
+   },
+   {description: "Meriam Library",
+    picture: "http://www.csuchico.edu/maps/campus/images/meriam.jpg",
+    from: "csuchico.edu"
+   },
+   {description: "Glenn Hall",
+    picture: "https://www.csuchico.edu/maps/images/buildings-photos/building-glenn.jpg",
+    from: "csuchico.edu"
+   }
+ ];
+
 var current;
+
 
 function shuffleGallery()
 {
@@ -8,16 +31,16 @@ function shuffleGallery()
   var randInt = Math.floor(Math.random() * len);
   //console.log(randInt);
   //console.log(current);
-  if (randInt == current)
-  {
+
     while(randInt == current)
     {
       randInt = Math.floor(Math.random() * len);
     }
-  }
+
 
   current = randInt;
   console.log(chico[randInt]);
-
-
+  document.getElementById("picture").src = chico[randInt].picture;
+  document.getElementById("caption").innerHTML = chico[randInt].description;
+  document.getElementById("source").innerHTML = chico[randInt].from;
 }
